89d610dde8be5414a3f3c16abfb130c2ed7c782a
Brienyll <Brienyll29@gmail.com>
2019-10-07 15:18:03 -0700

    merge pdf final commit

---
+++ pdf.py
@@ -0,0 +1,14 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+
+def pdf_combiner(list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in list:
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+
+pdf_combiner(inputs)
